f5bde82287715668e40702501258beb1af3825f4
Gerar22021 <Gerar2_yo@hotmail.com>
2024-10-15 20:48:35 -0300

    Actualizado_15/10

--- blog/apps/post/urls.py
+++ blog/apps/post/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import AgregarArticulo
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import AgregarArticulo, ActualizarArticulo, EliminarArticulo, ListarArticulo
 
 app_name = 'apps.post'
 
 urlpatterns = [
-    path('Articulo/', AgregarArticulo.as_view(), name = 'agregar_articulo'),
+    path('agregar_articulo/', AgregarArticulo.as_view(), name = 'agregar_articulo'),
+    path('actualizar_articulo/', ActualizarArticulo.as_view(), name = 'actualizar_articulo'),
+    path('eliminar_articulo/', EliminarArticulo.as_view(), name = 'eliminar_articulo'),
+    path('listar_articulo/', ListarArticulo.as_view(), name = 'listar_articulo'),
 ]
--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Articulo
 
 # Create your views here.
@@ -9,4 +10,26 @@ class AgregarArticulo(CreateView):
     template_name = 'articulos/agregar_articulo.html'
     fields = ['titulo','subtitulo','texto','categoria','imagen']
     success_url = reverse_lazy('index')
+
+class ActualizarArticulo(UpdateView):
+    model = Articulo
+    template_name = 'articulos/actualizar_articulo.html'
+    fields = ['titulo','subtitulo','texto','categoria','imagen']
+    success_url = reverse_lazy('index')
+
+class EliminarArticulo(DeleteView):
+    model = Articulo
+    template_name = 'articulos/Eliminar_articulo.html'
+    success_url = reverse_lazy('index')
+
+class ListarArticulo(ListView):
+    model = Articulo
+    template_name = 'articulos/listar_articulos.html'
+    context_object_name = 'articulos'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        articulos = Articulo.objects.all()
+        context['articulos'] = articulos
+        return context
     
